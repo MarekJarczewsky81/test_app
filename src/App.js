68019bdae0dadde16d1a8dd12dccf83906b76b1f
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-02-16 23:58:55 +0100

    Added Container component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Hero from "./components/Hero/Hero";
 import SearchForm from "./components/SearchForm/SearchForm";
 import List from "./components/List/List";
+import Container from "./components/Container/Container";
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
