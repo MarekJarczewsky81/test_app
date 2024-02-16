884bb81f2f8eab787717e080109ca085c6930ad1
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-02-16 22:41:00 +0100

    Added Font-Awesome to React

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/normalize.scss";
 import "./styles/global.scss";
+import "font-awesome/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
